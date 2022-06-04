07d7495c2b9098015b977b71137a85364e5cc063
Opu Hasnat <opqclick@gmail.com>
2022-06-04 20:09:40 +0600

    api done

---
+++ app/Http/Controllers/API/ShortLinkGeneratorController.php
@@ -0,0 +1,44 @@
+<?php
+
+namespace App\Http\Controllers\API;
+
+use App\Http\Controllers\Controller;
+use App\Models\ShortLink;
+use App\Rules\SafeBrowsing;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Validator;
+use Illuminate\Support\Str;
+
+class ShortLinkGeneratorController extends Controller
+{
+    public function store(Request $request){
+        $response = array('success'=>false, 'message' => '', 'response' => '');
+
+        //dd($response);
+
+        $validator = Validator::make($request->all(), [
+            'link' => ['required', 'url', new SafeBrowsing()]
+        ]);
+
+        //dd($validator->messages());
+
+        if ($validator->fails()) {
+            $response['response'] = $validator->messages();
+        } else {
+            $linkExists = ShortLink::where('link', $request->link)->first();
+
+            if($linkExists){
+                $response = ['success'=>true, 'message'=> 'Your provided link already exists', 'response' => $linkExists];
+            }else{
+                $shortLink = new ShortLink();
+                $shortLink->link = $request->link;
+                $shortLink->code = Str::random(6);
+                $shortLink->save();
+
+                $response = ['success'=>true, 'message'=> 'New short link created and link is verified by Google safe browsing API v4', 'response' => $shortLink];
+            }
+        }
+
+        return response()->json($response);
+    }
+}
--- routes/api.php
+++ routes/api.php
@@ -1,6 +1,6 @@
 <?php
 
-use Illuminate\Http\Request;
+use App\Http\Controllers\API\ShortLinkGeneratorController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -14,6 +14,5 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
-    return $request->user();
-});
+Route::post('/generate-short-link', [ShortLinkGeneratorController::class, 'store']);
+Route::get('/generate-short-link', [ShortLinkGeneratorController::class, 'store']);
